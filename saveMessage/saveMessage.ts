1ec0eccf67d72fe1062c150490b5eae8f2d4ac17
nidazehraa <nidazehra32@gmail.com>
2019-09-01 22:11:43 +0200

    saveMessage: return 200 from lambda function

--- saveMessage/saveMessage.ts
+++ saveMessage/saveMessage.ts
@@ -1,4 +1,19 @@
+import * as _ from 'lodash';
+
 export function handler(event, context, callback) {
-    console.log('test message');
-    callback(null);
+    const response = {
+       statusCode: 200,
+       body: JSON.stringify({
+         message: 'Success!',
+       }),
+     };
+    saveAndSendMessage(event)
+    .then(() => callback(null, response))
+    .catch(callback);
+}
+
+async function saveAndSendMessage(record) {
+    const message = JSON.parse(record.body);
+    console.log(message);
+    return Promise.resolve(null);
 }
