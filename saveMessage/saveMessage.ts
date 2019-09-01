ab904739b878564e1843703af4a67da2f1608b4a
nidazehraa <nidazehra32@gmail.com>
2019-09-01 23:02:29 +0200

    sendMessage: save message in dynamodb and also publish the message to sns topic

--- saveMessage/saveMessage.ts
+++ saveMessage/saveMessage.ts
@@ -1,6 +1,14 @@
 import * as _ from 'lodash';
+import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid/v4';
+
+AWS.config.update({
+    region: 'eu-central-1'
+});
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function handler(event, context, callback) {
+
     const response = {
        statusCode: 200,
        body: JSON.stringify({
@@ -14,11 +22,44 @@ export function handler(event, context, callback) {
 
 async function saveAndSendMessage(record) {
     const eventData = getEventData(record.body);
-    console.log(eventData);
-    return Promise.resolve(null);
+    await saveMessage(eventData);
+    await publishMessage(eventData);
 }
 
 function getEventData(event) {
     const message = JSON.parse(event);
     return _.get(message, 'event-data');
+}
+
+function saveMessage(message) {
+    const messageInfo = {
+        TableName: process.env.EMAILS_STATUS_TABLE,
+        Item: { id: uuid(), message: JSON.stringify(message) }
+    };
+    return new Promise((resolve, reject) => {
+        dynamoDb.put(messageInfo, (error, response) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(null);
+        });
+    });
+}
+
+function publishMessage(message) {
+    console.log('Publishing Event');
+    const sns = new AWS.SNS();
+    const params = {
+        Message: JSON.stringify(message),
+        Subject: "email status",
+        TopicArn: process.env.SNS_TOPIC
+    };
+    return new Promise((resolve, reject) => {
+        sns.publish(params, (error, response) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(null);
+        });
+    });
 }
